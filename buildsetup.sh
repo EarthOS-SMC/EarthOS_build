33dc956203733b7932972230a97e4e14be969228
adazem009 <68537469+adazem009@users.noreply.github.com>
2021-07-13 21:42:44 +0200

    buildsetup.sh: Fix path

--- buildsetup.sh
+++ buildsetup.sh
@@ -5,8 +5,8 @@ uname="EarthOS"
 release="2.1.0-wip-rc6" # EarthOS version
 build_date=$(date -u +"%m-%d-%Y-%H-%M")
 build_type="UNOFFICIAL"
-mkdir -p ../installer/rootfs/etc
-echo "$uname" > ../installer/rootfs/etc/release
-echo "$release" >> ../installer/rootfs/etc/release
-echo "${release}-${build_date}" >> ../installer/rootfs/etc/release
-echo $build_type >> ../installer/rootfs/etc/release
+mkdir -p installer/rootfs/etc
+echo "$uname" > installer/rootfs/etc/release
+echo "$release" >> installer/rootfs/etc/release
+echo "${release}-${build_date}" >> installer/rootfs/etc/release
+echo $build_type >> installer/rootfs/etc/release
