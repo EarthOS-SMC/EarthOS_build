7131fcbc38d50988e7d825fdb342b2fb97de3fc8
adazem009 <68537469+adazem009@users.noreply.github.com>
2021-07-31 14:08:13 +0200

    Release v2.1.0-wip-rc9

--- buildsetup.sh
+++ buildsetup.sh
@@ -2,7 +2,7 @@
 uname="EarthOS"
 
 # Release
-release="2.1.0-wip-rc8" # EarthOS version
+release="2.1.0-wip-rc9" # EarthOS version
 build_date=$(date -u +"%m-%d-%Y-%H-%M")
 build_type="UNOFFICIAL"
 mkdir -p installer/rootfs/etc
